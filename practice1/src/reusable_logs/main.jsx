ce4077e1df50586346f414a6b69a6df261eb7225
DanielTrusca1 <marian.trusca@8x8.com>
2025-07-30 13:58:15 +0300

    Create React Custom Hooks for functional components reusable logging

--- practice1/src/reusable_logs/main.jsx
+++ practice1/src/reusable_logs/main.jsx
@@ -1,5 +1,5 @@
 // IMPORTANT - Logs will be called 3 times in development mode due to React Strict Mode
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 
 // Dummy task function
 function log_something(msg) {
@@ -8,6 +8,15 @@ function log_something(msg) {
 
 // React functional component
 export function AF(props) {
+  useLogging("AF");
+  return <div></div>;
+}
+export function BF(props) {
+  useLogging("BF");
+  return <div></div>;
+}
+export function CF(props) {
+  useLogging("CF");
   return <div></div>;
 }
 
@@ -27,6 +36,18 @@ export class CC extends Component {
   render() {
     return <div></div>;
   }
+}
+
+// React Custom Hook Decorators
+export function useLogging(component) {
+  useEffect(() => {
+    log_something(`${component} mounted`);
+
+    // Clean-Up function
+    return () => {
+      log_something(`${component} unmounted`);
+    };
+  }, [component]);
 }
 
 // Higher-Order Component (HOC) Decorator
@@ -49,4 +70,3 @@ function with_logging(WrappedComponent) {
 export const LoggedAC = with_logging(AC);
 export const LoggedBC = with_logging(BC);
 export const LoggedCC = with_logging(CC);
-
